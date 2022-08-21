4798eff800752e59286d82889f3d61a250158961
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-08-21 16:25:36 +0400

    create filter component

---
+++ src/component/Filter/Filter.component.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+
+import { AiOutlineMenuUnfold , AiOutlineClose} from 'react-icons/ai';
+import { BsChevronDown } from 'react-icons/bs';
+import './Filter.style.scss';
+
+const animatedComponents = makeAnimated();
+
+const options = [
+  { value: 'Arcade', label: 'arcade' },
+  { value: 'Action', label: 'action' },
+  { value: 'Adventure', label: 'adventure' },
+];
+
+/** @namespace @component/Filter/Component */
+export default function Filter() {
+  const [isFilterActive, setFilterActive] = useState(false);
+  const [activeLangButton, setActiveLangButton] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const toggleActive = () => {
+    setFilterActive(isFilterActive => !isFilterActive);
+  }
+
+  const onSelect = (selectedOption: any) => {
+    setSelectedOption(selectedOption)
+  }
+
+  const onBtnClick = (e: any) => {
+    const btnIndex = e.target.dataset.indexNumber;
+    setActiveLangButton(Number(btnIndex));
+  }
+
+  return (
+    <div className={`filter ${isFilterActive ? 'filter-active' : ''}`}>
+      <button
+        className='filter-toggle'
+        onClick={toggleActive}
+        >
+        <AiOutlineMenuUnfold />
+      </button>
+      <div className='filter-content'>
+        <header className='filter-content-header'>
+          <img src="/assets/logo.svg" alt="gamefuz" />
+          <button onClick={toggleActive} className='icon'>
+            <AiOutlineClose />
+          </button>
+        </header>
+
+        <section className='filter-content-section'>
+          <h4>Genre:</h4>
+          <Select
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            value={selectedOption}
+            onChange={onSelect}
+            options={options}
+            isMulti
+          />
+        </section>
+    
+        <section className='filter-content-section'>
+          <h4>Year:</h4>
+          <input className='filter-content-section-range' type="range" />
+        </section>
+
+        <section className='filter-content-section'>
+          <h4>Language:</h4>
+          <div className='filter-content-section-btns'>
+            <button
+              onClick={onBtnClick}
+              data-index-number={0}
+              className={`btn-mini ${activeLangButton === 0 ? 'btn-mini-active' : ''}`}
+            >
+              Eng
+            </button>
+            <button
+              onClick={onBtnClick}
+              data-index-number={1}
+              className={`btn-mini ${activeLangButton === 1 ? 'btn-mini-active' : ''}`}
+            >
+              Rus
+            </button>
+            <button
+              onClick={onBtnClick}
+              data-index-number={2}
+              className={`btn-mini ${activeLangButton === 2 ? 'btn-mini-active' : ''}`}
+            >
+              Multi
+            </button>
+          </div>
+        </section>
+
+        <button className='filter-content-submit'>Filter</button>
+      </div>
+    </div>
+  )
+}
--- src/route/Homepage/Homepage.component.tsx
+++ src/route/Homepage/Homepage.component.tsx
@@ -2,12 +2,14 @@
 import Products from '@section/Products';
 import Companies from '@section/Companies';
 import Slider from '@component/Slider';
+import Filter from '@component/Filter';
 
 /** @namespace @route/Homepage/Component */
 export default function Homepage() {
   return (
     <>
       <Slider />
+      <Filter />
       <div className="container">
         <Products type="top" />
         <Products type="recent" />
