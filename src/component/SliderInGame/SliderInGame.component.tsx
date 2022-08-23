412862743d4e5516cdbac2883b4688c5cfbe3212
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-08-23 07:45:53 +0400

    building ingame slider

--- src/component/SliderInGame/SliderInGame.component.tsx
+++ src/component/SliderInGame/SliderInGame.component.tsx
@@ -5,34 +5,41 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import './SliderInGame.style.scss';
 import data from '@data/DetailsSlider.json';
 
+import Streamer from '@util/Streamer';
+
 /** @namespace @component/SliderInGame/Component */
 export default function SliderInGame() {
-  const { images } = data[0];
+  const { images, video } = data[0];
   const [current, setCurrent] = useState<number>(0);
 
   // go to the next slide
   const nextSlide = () => {
-    const isLastSlide = current === images.length - 1;
+    const isLastSlide = current === images.length;
     const newIndex = isLastSlide ? 0 : current + 1;
     setCurrent(newIndex);
   }
 
   // go to the previous slide
   const prevSlide = () => {
-    const newIndex = current === 0 ? 4 : current - 1;
+    const newIndex = current === 0 ? 5 : current - 1;
     setCurrent(newIndex);
   }
 
   // choose slide for your own
   const slideClick = (index:number) => {
-    setCurrent(index);
+    setCurrent(index + 1);
   }
 
   return (
-    <div className='slideringame'>
+    <div className='sliderin'>
+
+
+      
+      <Streamer className={current === 0 ? 'sliderin-video sliderin-video-active' : 'sliderin-video'} videoId={video} setCurrent={setCurrent} />
+
 
       {/* left and right buttons */}
-      <div className='slideringame-buttons'>
+      <div className='sliderin-buttons'>
         <div className='container'>
           <button className='left' onClick={prevSlide}>
             <BsChevronCompactLeft />
@@ -45,11 +52,12 @@ export default function SliderInGame() {
       </div>
 
       {/* posters */}
-      <div className='slideringame-posters'>
+      <div className='sliderin-posters'>
+
         {images.map(({poster, title}: any, index: any) => (
           <img
             key={index}
-            className={index === current ? 'active' : ''}
+            className={index + 1 === current ? 'active' : ''}
             src={poster}
             alt={title}
           />
@@ -58,12 +66,13 @@ export default function SliderInGame() {
 
 
       {/* counter with mini posters */}
-      <div className='slideringame-counter'>
+      <div className='sliderin-counter'>
         <div className="container">
+          <img className='yplay' src="/assets/yplay.png" alt="youtube play" />
           {images.map(({poster}: any, index: any) => (
             <img
               key={index}
-              className={index === current ? 'active' : ''}
+              className={index + 1 === current ? 'active' : ''}
               onClick={() => slideClick(index)}
               src={poster}
               alt="small"
