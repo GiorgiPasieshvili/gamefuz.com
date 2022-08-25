86734bfd0128de3c354ca1b5bc7f8b792a7675e9
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-08-25 20:13:46 +0400

    filter markup optimize

--- src/component/Filter/Filter.component.tsx
+++ src/component/Filter/Filter.component.tsx
@@ -1,14 +1,12 @@
 /* utilities import */
 import { useState } from "react";
 import Select from "react-select";
-import makeAnimated from "react-select/animated";
 /* assets & style import */
 import { AiOutlineMenuUnfold, AiOutlineClose } from "react-icons/ai";
-// import { BsChevronDown } from 'react-icons/bs';
 import "./Filter.style.scss";
-import options from "@data/Options.json";
+
+import genres from "@data/Genres.json";
 import { selectStyles } from "./Filter.config";
-const animatedComponents = makeAnimated();
 
 /** @namespace @component/Filter/Component */
 export default function Filter() {
@@ -21,87 +19,73 @@ export default function Filter() {
   };
 
   const onLangButtonClick = (e: any) => {
+    e.preventDefault();
     const btnIndex = e.target.dataset.indexNumber;
     setActiveLangButton(Number(btnIndex));
   };
 
   return (
-    <div className={isFilterOpen ? "filter filter-active" : "filter"}>
-      {/* filter open/close button */}
+    <div className="filter-parent">
       <button className="filter-toggle" onClick={toggleFilter}>
         <AiOutlineMenuUnfold />
       </button>
 
       {/* filter options - genre, year, lang */}
-      <div className="filter-content">
-        <div className="filter-content-top">
-          <header className="filter-content-top-header">
-            <img src="/images/logo.svg" alt="gamefuz" />
-            <button onClick={toggleFilter} className="icon">
-              <AiOutlineClose />
-            </button>
-          </header>
-
+      <aside className={`filter ${isFilterOpen ? "filter--active" : ""}`}>
+        <header className="filter__header">
+          <img className="filter__logo" src="/images/logo.svg" alt="gamefuz" />
+          <button className="filter__icon icon" onClick={toggleFilter}>
+            <AiOutlineClose />
+          </button>
+        </header>
+        <form className="filter__form">
           {/* select genres */}
-          <section className="filter-content-section">
-            <header>
-              <h4>Genre:</h4>
-            </header>
-            <Select
-              className="filter-content-select"
-              styles={selectStyles}
-              closeMenuOnSelect={false}
-              components={animatedComponents}
-              value={selectedGenre}
-              onChange={setSelectedGenre}
-              options={options}
-              isMulti
-            />
-          </section>
+          <label className="filter__label">Genre:</label>
+          <Select
+            className="filter__select"
+            styles={selectStyles}
+            closeMenuOnSelect={false}
+            value={selectedGenre}
+            onChange={setSelectedGenre}
+            options={genres}
+            isMulti
+          />
 
-          {/* year selection */}
-          <section className="filter-content-section">
-            <header>
-              <h4>Year:</h4>
-              <small>2020</small>
-            </header>
-            <input type="range" />
-          </section>
-
+          {/* choose year */}
+          <label className="filter__label">
+            Year: <span>2020</span>
+          </label>
+          <input type="range" />
           {/* choose language */}
-          <section className="filter-content-section">
-            <header>
-              <h4>Language:</h4>
-            </header>
-            <div className="mini-buttons">
-              <button
-                onClick={onLangButtonClick}
-                data-index-number={0}
-                className={activeLangButton === 0 ? "active" : ""}
-              >
-                Eng
-              </button>
-              <button
-                onClick={onLangButtonClick}
-                data-index-number={1}
-                className={activeLangButton === 1 ? "active" : ""}
-              >
-                Rus
-              </button>
-              <button
-                onClick={onLangButtonClick}
-                data-index-number={2}
-                className={activeLangButton === 2 ? "active" : ""}
-              >
-                Multi
-              </button>
-            </div>
-          </section>
-        </div>
 
-        {/* filter search button */}
-        <button className="filter-content-button primary-button">Filter</button>
-      </div>
+          <label className="filter__label">Language:</label>
+          <div className="mini-buttons">
+            <button
+              onClick={onLangButtonClick}
+              data-index-number={0}
+              className={activeLangButton === 0 ? "active" : ""}
+            >
+              Eng
+            </button>
+            <button
+              onClick={onLangButtonClick}
+              data-index-number={1}
+              className={activeLangButton === 1 ? "active" : ""}
+            >
+              Rus
+            </button>
+            <button
+              onClick={onLangButtonClick}
+              data-index-number={2}
+              className={activeLangButton === 2 ? "active" : ""}
+            >
+              Multi
+            </button>
+          </div>
+
+          <button className="filter__button primary-button">Filter</button>
+        </form>
+      </aside>
     </div>
   );
 }
