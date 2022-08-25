6ea7ebfde338407b6d4e4542e2c3470679ca3954
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-08-25 17:52:12 +0400

    details optimization

--- src/component/Details/Details.component.tsx
+++ src/component/Details/Details.component.tsx
@@ -27,161 +27,158 @@ export default function Details() {
   };
 
   return (
-    <section className="details section">
-      {/* left side - image and download */}
-      <div className="details__side details__side--left">
-        <img className="details__image" src={image} alt={title} />
-        <a className="details__link primary-button" href="/product/1">
+    <article className="details section">
+      <aside>
+        <img className="details__picture" src={image} alt={title} />
+        <a className="details__download primary-button" href="/product/1">
           Download
         </a>
-      </div>
+      </aside>
 
-      {/* right side - title, info, desc, specs */}
-      <div className="details__side details__side--right">
-        {/* top part of right side - title, indo, desc */}
-        <div>
-          {/* header of right side - game title */}
-          <header className="details__header details__header--default">
-            <h2 className="details__title">{title}</h2>
-            <small className="details__year">{year}</small>
-          </header>
-
-          {/* section of right side - game information */}
-          <section className="details__info details__section">
-            <h3>information on game:</h3>
-            {/* game genre */}
-            <div className="details__info--genre">
-              <small>genre: </small>
-              {categories.map((category, index) => (
-                <span key={index}>{category}</span>
-              ))}
-            </div>
-            {/* game creator */}
-            <div className="details__info--creator">
-              <small>creator: </small>
-              <span>{company}</span>
-            </div>
-            {/* game interface language */}
-            <div className="details__info--lang">
-              <small>interface language: </small>
-              {interface_lang.map((lang, index) => (
-                <span key={index}>{lang}</span>
-              ))}
-            </div>
-            {/* game dubbing language */}
-            <div className="details__info--lang">
-              <small>dubbing language: </small>
-              {dubbing_lang.map((lang, index) => (
-                <span key={index}>{lang}</span>
-              ))}
-            </div>
-            {/* game crack */}
-            <div className="details__info--crack">
-              <small>crack: </small>
-              <span>{crack}</span>
-            </div>
-          </section>
-
-          {/* section of right side - game description */}
-          <section className="details__description details__section">
-            <h3>description:</h3>
-            <p>{description}</p>
-          </section>
-        </div>
-
-        {/* bottom part of right side - system requirements */}
-        <section className="details__requirements details__section">
-          <header className="details__header details__header--secondary">
-            <h3>system requirements:</h3>
-            <div className="mini-buttons">
-              <button
-                onClick={onButtonClick}
-                data-index-number={0}
-                className={activeButton === 0 ? "active" : ""}
-              >
-                Low
-              </button>
-              <button
-                onClick={onButtonClick}
-                data-index-number={1}
-                className={activeButton === 1 ? "active" : ""}
-              >
-                Best
-              </button>
-            </div>
-          </header>
-          {/* low system requirements */}
-          <ul
-            className={
-              activeButton === 0
-                ? "details__list details__list--active"
-                : "details__list"
-            }
-          >
-            <li>
-              <small>OS: </small>
-              <span>{specs.low.os}</span>
-            </li>
-            <li>
-              <small>CPU: </small>
-              <span>{specs.low.cpu}</span>
-            </li>
-            <li>
-              <small>RAM: </small>
-              <span>{specs.low.ram}</span>
-            </li>
-            <li>
-              <small>GPU: </small>
-              <span>{specs.low.gpu}</span>
-            </li>
-            <li>
-              <small>Sound Card: </small>
-              <span>{specs.low.sound}</span>
-            </li>
-            <li>
-              <small>Free Space: </small>
-              <span>{specs.low.space}</span>
-            </li>
-          </ul>
-          {/* best system requirements */}
-          <ul
-            className={
-              activeButton === 1
-                ? "details__list details__list--active"
-                : "details__list"
-            }
-          >
-            <li>
-              <small>OS: </small>
-              <span>{specs.best.os}</span>
-            </li>
-            <li>
-              <small>CPU: </small>
-              <span>{specs.best.cpu}</span>
-            </li>
-            <li>
-              <small>RAM: </small>
-              <span>{specs.best.ram}</span>
-            </li>
-            <li>
-              <small>GPU: </small>
-              <span>{specs.best.gpu}</span>
-            </li>
-            <li>
-              <small>Sound Card: </small>
-              <span>{specs.best.sound}</span>
-            </li>
-            <li>
-              <small>Free Space: </small>
-              <span>{specs.best.space}</span>
-            </li>
-          </ul>
-        </section>
-
+      <header>
+        <h2 className="details__heading details__heading--big">
+          {title}
+          <span>{year}</span>
+        </h2>
         <div className="details__icon">
           <AiFillWindows />
         </div>
-      </div>
-    </section>
+      </header>
+
+      <main>
+        <section>
+          <h3 className="details__heading details__heading--small">
+            information on game:
+          </h3>
+          <ul className="details__list">
+            <li>
+              Genre:
+              {categories.map((category, index) => (
+                <span className="details__comma" key={index}>
+                  {category}
+                </span>
+              ))}
+            </li>
+            <li>
+              Creator:
+              <span>{company}</span>
+            </li>
+            <li>
+              Interface Language:
+              {interface_lang.map((lang, index) => (
+                <span className="details__bar" key={index}>
+                  {lang}
+                </span>
+              ))}
+            </li>
+            <li>
+              Dubbing Language:
+              {dubbing_lang.map((lang, index) => (
+                <span className="details__bar" key={index}>
+                  {lang}
+                </span>
+              ))}
+            </li>
+            <li>
+              Crack:
+              <span>{crack}</span>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h3 className="details__heading details__heading--small">
+            description:
+          </h3>
+          <p className="details__paragraph">{description}</p>
+        </section>
+      </main>
+
+      <footer>
+        <div className="details__flex">
+          <h3 className="details__heading details__heading--small">
+            system requirements:
+          </h3>
+          <div className="mini-buttons">
+            <button
+              onClick={onButtonClick}
+              data-index-number={0}
+              className={activeButton === 0 ? "active" : ""}
+            >
+              Low
+            </button>
+            <button
+              onClick={onButtonClick}
+              data-index-number={1}
+              className={activeButton === 1 ? "active" : ""}
+            >
+              Best
+            </button>
+          </div>
+        </div>
+
+        {/* low system requirements */}
+        <ul
+          className={`details__list details__list--secondary ${
+            activeButton === 0 ? "details__list--active" : ""
+          }`}
+        >
+          <li>
+            OS:
+            <span>{specs.low.os}</span>
+          </li>
+          <li>
+            CPU:
+            <span>{specs.low.cpu}</span>
+          </li>
+          <li>
+            RAM:
+            <span>{specs.low.ram}</span>
+          </li>
+          <li>
+            GPU:
+            <span>{specs.low.gpu}</span>
+          </li>
+          <li>
+            Sound Card:
+            <span>{specs.low.sound}</span>
+          </li>
+          <li>
+            Free Space:
+            <span>{specs.low.space}</span>
+          </li>
+        </ul>
+        {/* best system requirements */}
+        <ul
+          className={`details__list details__list--secondary ${
+            activeButton === 1 ? "details__list--active" : ""
+          }`}
+        >
+          <li>
+            OS:
+            <span>{specs.best.os}</span>
+          </li>
+          <li>
+            CPU:
+            <span>{specs.best.cpu}</span>
+          </li>
+          <li>
+            RAM:
+            <span>{specs.best.ram}</span>
+          </li>
+          <li>
+            GPU:
+            <span>{specs.best.gpu}</span>
+          </li>
+          <li>
+            Sound Card:
+            <span>{specs.best.sound}</span>
+          </li>
+          <li>
+            Free Space:
+            <span>{specs.best.space}</span>
+          </li>
+        </ul>
+      </footer>
+    </article>
   );
 }
